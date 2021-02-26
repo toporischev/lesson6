d048047216416c677e0a6dd6e578b64783f3b382
Toporishev Ivan <toporishev.i@medindex.ru>
2021-02-26 21:50:59 +0300

    Добавлен user сервис

---
+++ UserService/Startup.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Npgsql;
+
+namespace UserService
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddScoped(_ => new NpgsqlConnection(_configuration.GetValue<string>("connectionString")));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ UserService/UserController.cs
@@ -0,0 +1,82 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace UserService
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly NpgsqlConnection _connection;
+        
+        public UserController(NpgsqlConnection connection)
+        {
+            _connection = connection;
+        }
+        
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<User>> Get(int userId)
+        {
+            const string sql = @"select * from users where Id = userId";
+
+            var user = await _connection.QueryFirstOrDefaultAsync<User>(sql, userId);
+            if (user is null)
+                return NotFound();
+            
+            return user;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User[]>> Get()
+        {
+            const string sql = @"select * from users";
+
+            var users = await _connection.QueryAsync<User>(sql);
+            return users.ToArray();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(User user)
+        {
+            const string sql = @"
+                insert into Users (email, last_name, first_name)
+                values (@email, @lastName, @firstName) returning id";
+            
+            var userId = await _connection.QuerySingleAsync<int>(sql, user);
+            return CreatedAtAction(nameof(Get), new {id = userId}, userId);
+        }
+
+        [HttpPut("{userId}")]
+        public async Task<IActionResult> Update(int userId, User user)
+        {
+            const string sql = @"
+                update Users
+                set
+                    email = @email,
+                    lastName = @lastName,
+                    firstName = @firstName
+                where id = @userId";
+            
+            var rowsAffected = await _connection.ExecuteAsync(sql, new { id = userId, user = user});
+            if (rowsAffected == 0)
+                return NotFound();
+
+            return NoContent();
+        }
+        
+        [HttpDelete("{userId}")]
+        public async Task<IActionResult> Delete(int userId)
+        {
+            const string sql = @"delete from Users where id = @userId";
+            
+            var rowsAffected = await _connection.ExecuteAsync(sql, userId);
+            if (rowsAffected == 0)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
